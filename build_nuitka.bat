0c6c0fa81305d7d3031b8bb4b2facf87671f6d26
Hirota <ubonira@gmail.com>
2026-09-23 19:16:42 +0900

    Set Nuitka Windows file and product versions

--- build_nuitka.bat
+++ build_nuitka.bat
@@ -1,5 +1,6 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-python -m nuitka --standalone --remove-output --assume-yes-for-downloads --jobs=2 --enable-plugin=pyside6 --include-module=win32timezone --windows-console-mode=disable --windows-icon-from-ico=assets\icons\MailPDFViewer.ico --windows-product-name=MailPDFViewer --windows-file-description="Mail PDF Viewer" --output-filename=MailPDFViewer.exe main.py
+if not defined APP_VERSION set /p APP_VERSION=<VERSION
+python -m nuitka --standalone --remove-output --assume-yes-for-downloads --jobs=2 --enable-plugin=pyside6 --include-module=win32timezone --windows-console-mode=disable --windows-icon-from-ico=assets\icons\MailPDFViewer.ico --windows-product-name=MailPDFViewer --windows-file-description="Mail PDF Viewer" --windows-file-version=%APP_VERSION%.0 --windows-product-version=%APP_VERSION%.0 --output-filename=MailPDFViewer.exe main.py
 exit /b %errorlevel%
