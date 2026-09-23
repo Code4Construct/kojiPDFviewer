687d0ee1d1b2ba90a106873e610101e5249b6a9c
Hirota <ubonira@gmail.com>
2026-09-23 19:52:59 +0900

    Disable LTO to avoid MSVC heap exhaustion

--- build_nuitka.bat
+++ build_nuitka.bat
@@ -2,5 +2,5 @@
 setlocal
 cd /d "%~dp0"
 if not defined APP_VERSION set /p APP_VERSION=<VERSION
-python -m nuitka --standalone --remove-output --assume-yes-for-downloads --jobs=2 --enable-plugin=pyside6 --include-module=win32timezone --windows-console-mode=disable --windows-icon-from-ico=assets\icons\kojiPDFviewer.ico --windows-product-name=kojiPDFviewer --windows-file-description="kojiPDFviewer" --windows-file-version=%APP_VERSION%.0 --windows-product-version=%APP_VERSION%.0 --output-filename=kojiPDFviewer.exe main.py
+python -m nuitka --standalone --remove-output --assume-yes-for-downloads --jobs=2 --lto=no --enable-plugin=pyside6 --include-module=win32timezone --windows-console-mode=disable --windows-icon-from-ico=assets\icons\kojiPDFviewer.ico --windows-product-name=kojiPDFviewer --windows-file-description="kojiPDFviewer" --windows-file-version=%APP_VERSION%.0 --windows-product-version=%APP_VERSION%.0 --output-filename=kojiPDFviewer.exe main.py
 exit /b %errorlevel%
